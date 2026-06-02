74c02fbde6fafa156d1a936a9022faa6d75e3221
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-06-02 03:12:59 +0200

    feat: rescan on zkool fix: sending to extracted addresses fix: labeling shielding txs fix: autoshield endless loop fix: autoshield t addr not firing fix: autoshield destinations chore: remove old routes for rescan with zkool other minor fixes/cleanups

--- lib/view_model/dashboard/transaction_list_item.dart
+++ lib/view_model/dashboard/transaction_list_item.dart
@@ -66,8 +66,13 @@ class TransactionListItem extends ActionListItem with Keyable {
   }
 
   String get formattedTitle {
-    if (transaction.additionalInfo['autoShield'] == true) {
-      return "Autoshield";
+    if (transaction.additionalInfo['isAutoShield'] == true) {
+      if (transaction.isPending) {
+        final status = formattedStatus;
+        final baseString = S.current.shielding;
+        return status.isNotEmpty ? "$baseString $status" : "$baseString...";
+      }
+      return S.current.shielding;
     }
     if (transaction.isPending) {
       final status = formattedStatus;
